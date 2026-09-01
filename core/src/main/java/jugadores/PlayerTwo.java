b0a8659ea70aee288ad3d49d45bf5a3adf1244bb
Hane-Xel <bartolome.helena.et35.2026@gmail.com>
2026-09-01 14:42:06 -0300

    Implementar PlayerTwo con conexión a personaje y controles

--- core/src/main/java/jugadores/PlayerTwo.java
+++ core/src/main/java/jugadores/PlayerTwo.java
@@ -1,5 +1,68 @@
 package jugadores;
 
-public class PlayerTwo {
+import personajes.Aves;
+import controles.Controles;
 
+public class PlayerTwo {
+	private Aves personaje;
+	private Controles controles;
+	private static final float VELOCIDAD_MOVIMIENTO = 5f;
+	
+	public PlayerTwo(Aves personaje, Controles controles) {
+		this.personaje = personaje;
+		this.controles = controles;
+	}
+	
+	/**
+	 * Actualiza el estado del jugador procesando los controles
+	 */
+	public void actualizar() {
+		// Procesar movimiento
+		boolean[] movimientos = controles.obtenerMovimientoJugador2();
+		personaje.procesarMovimiento(movimientos, VELOCIDAD_MOVIMIENTO);
+		
+		// Procesar acciones de combate
+		boolean[] acciones = controles.obtenerAccionesJugador2();
+		personaje.procesarAcciones(acciones);
+	}
+	
+	/**
+	 * Obtiene el personaje del jugador
+	 * @return el personaje
+	 */
+	public Aves getPersonaje() {
+		return personaje;
+	}
+	
+	/**
+	 * Establece el personaje del jugador
+	 * @param personaje el nuevo personaje
+	 */
+	public void setPersonaje(Aves personaje) {
+		this.personaje = personaje;
+	}
+	
+	/**
+	 * Obtiene los controles del jugador
+	 * @return los controles
+	 */
+	public Controles getControles() {
+		return controles;
+	}
+	
+	/**
+	 * Establece los controles del jugador
+	 * @param controles los nuevos controles
+	 */
+	public void setControles(Controles controles) {
+		this.controles = controles;
+	}
+	
+	/**
+	 * Obtiene la velocidad de movimiento del jugador
+	 * @return la velocidad de movimiento
+	 */
+	public float getVelocidadMovimiento() {
+		return VELOCIDAD_MOVIMIENTO;
+	}
 }
